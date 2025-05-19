a68621f8a94498ad3d5e0637631cde42efbea834
Jason Salazar <razalasnosaj@gmail.com>
2025-05-19 08:15:58 +0800

    Cloudinary, ng2-file-upload, related entities, updating and deleting resources, REST API return

--- DatingApp/Controllers/AccountController.cs
+++ DatingApp/Controllers/AccountController.cs
@@ -42,14 +42,16 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await context.Users
-                .FirstOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
-            
+                .Include(p => p.Photos)
+                .FirstOrDefaultAsync(x =>
+                    x.UserName.ToLower() == loginDto.Username.ToLower());
+
             if (user == null) return Unauthorized("Invalid username");
-            
+
             using var hmac = new HMACSHA512(user.PasswordSalt);
-            
+
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-            
+
             for (int i = 0; i < computedHash.Length; i++)
             {
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
@@ -58,7 +60,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = tokenService.CreateToken(user)
+                Token = tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
