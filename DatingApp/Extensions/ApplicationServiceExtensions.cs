151630c35c7289a418b2b172a3484316d4998d5d
Jason Salazar <razalasnosaj@gmail.com>
2025-06-03 22:51:22 +0800

    Use and set up SignalR on both API and NG, implement online presence, implement live chat between users, dealing with UTC date format

--- DatingApp/Extensions/ApplicationServiceExtensions.cs
+++ DatingApp/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -22,6 +23,8 @@ namespace API.Extensions
             services.AddScoped<LogUserActivity>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddSignalR();
+            services.AddSingleton<PresenceTracker>();
 
             return services;
         }
--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:7003/api/',
+  production: false,
+  apiUrl: 'https://localhost:7003/api/',
+  hubsUrl: 'https://localhost:7003/hubs/',
 };
